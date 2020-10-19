62f7cd338f65c3b080790a7e775c61a6bbec7b1c
Emily-Eckstein <70826450+Emily-Eckstein@users.noreply.github.com>
2020-10-19 00:08:49 -0500

    Create Glop.cs

---
+++ Glop/Assets/Glop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glop : MonoBehaviour
+{
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public BoxCollider2D col;
+
+    [HideInInspector]public Vector3 pos
+    {
+        get { return transform.position; }
+    }
+
+     void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+
+    }
+
+    public void Push(Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+
+    }
+
+    public void ActivateRb()
+    {
+        rb.isKinematic = false;
+
+    }
+    public void DeactivateRb()
+       
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0f;
+        rb.isKinematic = true;
+    }
+}
