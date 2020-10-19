05773f347e89c8900677b7d5753e4715bb881abe
Emily-Eckstein <70826450+Emily-Eckstein@users.noreply.github.com>
2020-10-19 00:08:36 -0500

    Create GameManager.cs
    Added glop movement and trajectory

---
+++ Glop/Assets/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton class: GameManager
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+        }
+    }
+    #endregion
+
+    Camera cam;
+
+    public Glop glop;
+    public Trajectory trajectory;
+    [SerializeField] float pushForce = 4f;
+
+    bool isDragging = false;
+
+    Vector2 startPoint;
+    Vector2 endPoint;
+    Vector2 direction;
+    Vector2 force;
+    float distance;
+
+    void Start()
+    {
+        cam = Camera.main;
+        glop.DeactivateRb();
+
+    }
+
+    void Update()
+    {
+       if (Input.GetMouseButtonDown (0))
+        {
+            isDragging = true;
+            OnDragStart();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            OnDragEnd();
+        }
+
+        if (isDragging)
+        {
+            OnDrag();
+        }
+    }
+
+    //Drag
+
+    void OnDragStart()
+    {
+        glop.DeactivateRb();
+        startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        trajectory.Show();
+    }
+     void OnDrag()
+    {
+        endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+        distance = Vector2.Distance(startPoint, endPoint);
+        direction = (startPoint - endPoint).normalized;
+        force = direction * distance * pushForce;
+
+
+        //just for debug
+        Debug.DrawLine(startPoint, endPoint);
+
+        trajectory.UpdateDots(glop.pos, force);
+    }
+    void OnDragEnd()
+    {
+        //push glop
+        glop.ActivateRb();
+
+        glop.Push(force);
+
+        trajectory.Hide();
+    }
+}
