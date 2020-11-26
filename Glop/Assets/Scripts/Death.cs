2d5362d50041083ee4867f9c22ed68430c1f7fc0
HunterDavis116 <htdavis@eagles.usi.edu>
2020-11-26 17:46:10 -0600

    added level 4 and Out of bounds/death scripts

---
+++ Glop/Assets/Scripts/Death.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    public Rigidbody2D glopBody;
+
+    void Start()
+    {
+        gameObject.AddComponent<BoxCollider2D>();
+        glopBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death")
+        {
+            Debug.Log("CollisionEntered");
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
