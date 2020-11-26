30efd80175734c0cd06d89ff66bd21e13eaaa575
EthanPartridge98 <eepartridg@eagles.usi.edu>
2020-11-25 21:33:13 -0600

    Level Transitions
    ayo

---
+++ Glop/Assets/LevelControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int index;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("glop"))
+        {
+            //Loading level with build index
+            SceneManager.LoadScene(index);
+        }
+    }
+}
