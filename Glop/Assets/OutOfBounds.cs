1b433ca8d21d51ae256054594f56e44d42a1e2e6
EthanPartridge98 <eepartridg@eagles.usi.edu>
2020-11-27 15:32:37 -0600

    Death Screen

---
+++ Glop/Assets/OutOfBounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutOfBounds : MonoBehaviour
+{
+    public int i;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("glop"))
+        {
+            SceneManager.LoadScene(i);
+        }
+    }
+}
---
+++ Glop/Assets/spike.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spike : MonoBehaviour
+{
+    public int spikeindex;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("glop"))
+        {
+            SceneManager.LoadScene(spikeindex);
+        }
+    }
+}
