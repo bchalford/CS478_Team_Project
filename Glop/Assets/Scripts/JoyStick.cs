4d3a876cc206e4f3ec95fe1eff72322dadca91c9
Emily-Eckstein <70826450+Emily-Eckstein@users.noreply.github.com>
2020-11-28 04:06:26 -0500

    Added joystick movement

---
+++ Glop/Assets/Scripts/JoyStick.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStick : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5.0f;
+
+
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+
+    public Transform circle;
+    public Transform outerCircle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+
+           
+            circle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            touchStart = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else
+        {
+            touchStart = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 3.0f); //limits joystick drag
+            moveCharacter(direction * 1);
+
+            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y) * 1;
+        }
+        //makes the joystick stay on screen and not disapear
+        else
+        {
+            circle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+}
