8dd004979e60f94730fcea8ea8ebe0f0cb880f69
Blaine Halford <blainehalford1993@gmail.com>
2020-11-29 13:00:06 -0600

    Retry on Commit for Pause Menu
    Signed-off-by: Blaine Halford <blainehalford1993@gmail.com>

---
+++ Glop/Assets/Scripts/PauseMenuV2.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuV2 : MonoBehaviour
+{
+	public static bool GameIsPaused = false;
+	public GameObject pauseMenuUI;
+	//public int load;
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+			if (GameIsPaused)
+            {
+				ResumeGame();
+            } else
+            {
+				PauseGame();
+            }
+        }
+    }
+	public void ResumeGame ()
+    {
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+	void PauseGame ()
+    {
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+    }
+	public void Restart ()
+    {
+		Time.timeScale = 1f;
+		Debug.Log("Restarting");
+		//SceneManager.LoadScene("Level0");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+	public void QuitLevel ()
+    {
+		Time.timeScale = 1f;
+		Debug.Log("Quitting");
+		SceneManager.LoadScene("Main Menu (real)");
+
+    }
+}
+	/*GameObject[] pauseObjects;
+
+	// Use this for initialization
+	void Start()
+	{
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		//uses the p button to pause and unpause the game
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			}
+			else if (Time.timeScale == 0)
+			{
+				Debug.Log("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+	}
+
+
+	//Reloads the Level
+	public void Reload()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	//controls the pausing of the scene
+	public void pauseControl()
+	{
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+		}
+		else if (Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			hidePaused();
+		}
+	}
+
+	//shows objects with ShowOnPause tag
+	public void showPaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(true);
+		}
+	}
+
+	//hides objects with ShowOnPause tag
+	public void hidePaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(false);
+		}
+	}
+
+	//loads inputted level
+	public void LoadLevel(string level)
+	{
+		Application.LoadLevel(level);
+	}
+
+}*/
